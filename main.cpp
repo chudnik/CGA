35e14cc08d6a8ca32ddcd266c11552e7374248bb
chudaev.nn <chudaev.nn@dozen.mephi.ru>
2024-07-26 16:04:26 +0300

    Added a deck and the ability to interact with cards in a player

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,7 +9,7 @@ class Card {
     enum Rank { ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
     enum Color { RED, BLACK };
 
-    Card(Suit s, Rank r) : suit(s), rank(r) { color = static_cast<Color>(suit / 2); }
+    Card(Suit s, Rank r) : suit(s), rank(r) { color = (suit == HEARTS || suit == DIAMONDS) ? RED : BLACK; }
 
     Suit getSuit() const { return suit; }
     Rank getRank() const { return rank; }
@@ -20,15 +21,58 @@ class Card {
         cout << ranks[rank - 1] << " of " << suits[suit] << endl;
     }
 
+    bool operator==(const Card &other) const { return suit == other.suit && rank == other.rank; }
+
    private:
     Suit suit;
     Rank rank;
     Color color;
 };
 
+class Deck {
+   public:
+    Deck() : countCards(52) {
+        for (int s = Card::HEARTS; s < Card::SPADES; s++) {
+            for (int r = Card::ACE; r < Card::KING; r++) {
+                cards.push_back(Card(static_cast<Card::Suit>(s), static_cast<Card::Rank>(r)));
+            }
+        }
+    }
+
+    void removeCard(const Card &card) {
+        auto it = find(cards.begin(), cards.end(), card);
+        if (it != cards.end()) {
+            cards.erase(it);
+            countCards--;
+        }
+    }
+
+   private:
+    unsigned int countCards;
+    vector<Card> cards;
+};
+
 class Player {
    public:
-    Player(const string n) : name(n) {}
+    Player(const string n) : name(n), countCards(0) {}
+
+    string getName() const { return name; }
+    unsigned int getCountCards() const { return countCards; }
+    vector<Card> getHand() const { return hand; }
+
+    void addCard(const Card &card) {
+        hand.push_back(card);
+        countCards++;
+    }
+
+    void removeCard(const Card &card) {
+        auto it = find(hand.begin(), hand.end(), card);
+        if (it != hand.end()) {
+            hand.erase(it);
+            countCards--;
+        }
+    }
+
     void printHand() const {
         cout << name << "'s hand:" << endl;
         for (const auto &card : hand) {
@@ -38,6 +82,7 @@ class Player {
 
    private:
     string name;
+    unsigned int countCards;
     vector<Card> hand;
 };
 
@@ -45,9 +90,16 @@ class Game {
    public:
     enum GameType { POKER, BLACKJACK, FOOL };
 
+    Game(GameType type, unsigned int count) : gameType(type), countPlayers(count) {}
+
+    GameType getGameType() const { return gameType; }
+    unsigned int getCountPlayers() const { return countPlayers; }
+    vector<Player> getPlayers() const { return players; }
+
    private:
     GameType gameType;
-    vector<>
+    unsigned int countPlayers;
+    vector<Player> players;
 };
 
 int main() {
